0eebd365b7af23634013794d1d095c7d53e238b8
Ilana Bonder <ilanabonder@gmail.com>
2016-06-30 10:17:41 +0300

    version 2 with corrected changes from wdana notes

--- Assignmentnew.js
+++ Assignmentnew.js
@@ -1,6 +1,4 @@
-/**
- * Created by itc_user on 6/18/2016.
- */
+
 
 /*function to print name along with the Quiz result popup page */
 var printName = function () {
@@ -19,68 +17,50 @@ var checkScore  = function() {
     score += parseInt(document.getElementById("q3").value);
     score += checkAnswer("q4");
     score += parseInt(document.getElementById("q5").value);
+    printName();
 
 
     /*lowest score = israeli brunch*/
 
     if (score >= 0 && score <= 8) {
         var modalbreakfast = document.getElementById('myModalBreakfast');
-        var span = document.getElementsByClassName("close")[0];
+        var span3 = document.getElementsByClassName("close")[3];
         modalbreakfast.style.display = "block";
-        span.onclick = function () {
+        span3.onclick = function () {
             modalbreakfast.style.display = "none";
         }
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modalbreakfast.style.display = "none";
-            }
-        }
     }
+
     /*second score = shakshooka*/
     else if (score > 8 && score <= 11) {
         var modalshakshooka = document.getElementById('myModalShakshooka');
-        var span = document.getElementsByClassName("close")[0];
+        var span2 = document.getElementsByClassName("close")[2];
         modalshakshooka.style.display = "block";
-        span.onclick = function () {
+        span2.onclick = function () {
             modalshakshooka.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modalshakshooka.style.display = "none";
-            }
         }
     }
     /*third score = falafel*/
     else if (score > 11 && score <= 13) {
         var modalfalafel = document.getElementById('myModalFalafel');
-        var span = document.getElementsByClassName("close")[0];
+        var span1 = document.getElementsByClassName("close")[1];
 
         modalfalafel.style.display = "block";
-        span.onclick = function () {
+        span1.onclick = function () {
             modalfalafel.style.display = "none";
         }
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modalfalafel.style.display = "none";
-            }
         }
-    }
 
     /*highest score = bureka*/
-    if (score > 13 && score <= 16) {
+    else if (score > 13 && score <= 16) {
         var modalbureka = document.getElementById('myModalBureka');
         var span = document.getElementsByClassName("close")[0];
-
         modalbureka.style.display = "block";
         span.onclick = function () {
             modalbureka.style.display = "none";
         }
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modalbureka.style.display = "none";
-            }
-        }
-    }
+        
+}
 };
 
 var checkAnswer = function(name) {
@@ -92,4 +72,4 @@ var checkAnswer = function(name) {
         }
     }
     return answerScore;
-}
+};
